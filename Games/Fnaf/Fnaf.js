e769dd40b0508351e40fd9acc560e98544cd932a
beni529 <Beniflash@Icloud.com>
2026-03-27 11:52:58 -0700

    Fnaf 1-4

---
+++ Games/Fnaf/Fnaf.js
@@ -0,0 +1,28 @@
+const gamePane = document.getElementById("gameGrid");
+const gameNames = [
+    "Fnaf1",
+    "Fnaf2",
+    "Fnaf3",
+    "Fnaf4"
+];
+
+function gameImage(gameName) {
+    return `./${gameName}/${gameName}.png`;
+}
+
+function gamePage(gameName) {
+    return `./${gameName}/${gameName}.html`;
+}
+
+function makeEntry(gameName) {
+    const img = document.createElement("img");
+    img.src=gameImage(gameName);
+    img.onclick= () => {window.location.href = gamePage(gameName)};
+    img.alt=gameName;
+    img.className="gameCover";
+    gamePane.append(img);
+}
+
+for(let i=0;i<gameNames.length;i++) {
+    makeEntry(gameNames[i]);
+}
